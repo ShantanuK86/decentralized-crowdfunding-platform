0d89d501f3bdc0bacee4295a27868abfa44813c9
ShantanuK86 <sk9411011994@gmail.com>
2023-11-18 09:59:15 +0530

    changes

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -2,7 +2,7 @@ export default function ConnectWallet(props) {
     return (
         <div className='connectWallet'>
             <div className='typingContainer'>
-                <div className='typing'>DeFi</div>
+                <div className='typing'>DeFiStarter</div>
             </div>
             <div className="walletButtonContainer">
                 <button className='walletButton' onClick={props.connectMetamask}>
